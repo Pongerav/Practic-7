0e179c5daba3547faced941882bca6eea5368adb
Pavel Ast a.k.a Pong <pavel.astashkin.05@mail.ru>
2022-09-24 09:35:49 +0300

    Third exercise

---
+++ 003_Triple_Massiv/Program.cs
@@ -0,0 +1,42 @@
+﻿void FillArray(int[,,] array)
+{
+    for (int i = 0; i <= array.GetUpperBound(0); i += 1)
+    {
+        for (int j = 0; j <= array.GetUpperBound(1); j += 1)
+        {
+            for (int n = 0; n <= array.GetUpperBound(2); n += 1)
+            {
+                array[i,j,n] = new Random().Next(10,100);
+            }
+        }
+    }
+}
+void PrintArray(int[,,] array)
+{
+   for (int i = 0; i <= array.GetUpperBound(1); i += 1)
+    {  
+        Console.WriteLine();
+        for (int j = 0; j <= array.GetUpperBound(0); j += 1)
+        {
+            Console.Write($"\t");
+            for (int n = 0; n <= array.GetUpperBound(2); n++)
+            {
+                Console.Write(array[j,i,n] + " ");
+            }
+        }
+    } 
+}
+
+Console.WriteLine("Введите кол-во двумерных массивов");
+int countmas = int.Parse(Console.ReadLine() ?? "0");
+
+Console.WriteLine("Введите кол-во строк в двумерном массиве");
+int countrows = int.Parse(Console.ReadLine() ?? "0");
+
+Console.WriteLine("Введите кол-во столбцов в двумерном массиве");
+int countcollumns = int.Parse(Console.ReadLine() ?? "0");
+
+int[,,] array = new int[countmas , countrows , countcollumns];
+
+FillArray(array);
+PrintArray(array);
